552253b807432e7963fb98e7619513707d2586f4
Shirley <shirley_cinderella@outlook.com>
2019-09-22 22:14:29 +0800

    collection cell

---
+++ appshop/View/ProductCell.swift
@@ -0,0 +1,16 @@
+//
+//  ProductCell.swift
+//  appshop
+//
+//  Created by Dante on 2019/9/22.
+//  Copyright © 2019 cinderella. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+}
