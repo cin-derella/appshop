b07b0ecf5277af12976f0d24aeae8cc2da039933
cin-derella <shirley_cinderella@outlook.com>
2019-09-24 23:07:53 +0800

    products show

--- appshop/Controller/ProductsVC.swift
+++ appshop/Controller/ProductsVC.swift
@@ -8,23 +8,52 @@
 
 import UIKit
 
-class ProductsVC: UIViewController {
+class ProductsVC: UIViewController ,UICollectionViewDelegate,UICollectionViewDataSource{
+  
+    
+    @IBOutlet weak var productsCollection:UICollectionView!
 
+    @IBOutlet weak var VCName: UILabel!
+    var categoryName: String = ""
+    
+    
     private(set) public var products = [Product]()
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+       productsCollection.dataSource = self
+       productsCollection.delegate = self
     }
     
 
     func initProducts(category:Category){
         products = DataService.instance.getProducts(forCategoryTitle: category.title)
         
+        navigationItem.title=category.title
+        self.categoryName = category.title
+        //VCName = UILabel()
         
     }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        VCName.text = "I Love " + self.categoryName + "!"
+        return products.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ProductCell",for:indexPath)as? ProductCell
+        if cell != nil {
+            let product = products[indexPath.row]
+            cell?.updateViews(product:product)
+            
+            return cell!
+            
+        }
+        return ProductCell()
+    }
+    
 
     
 }
